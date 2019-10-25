ec01c018981f71820e9fc7ab87507edb5530a45b
ELLOWE <47371493+ELLowe@users.noreply.github.com>
2019-10-24 21:09:12 -0500

    work in progress

--- static/js/embed_Tdash.js
+++ static/js/embed_Tdash.js
@@ -1,6 +1,6 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-        url = "https://public.tableau.com/views/NYCitiBikeProgramJanuary2019Analysis/Dashboard2?:embed=y&:display_count=yes&:origin=viz_share_link",
+        url = "https://public.tableau.com/views/FinalProjectVisualizations_15719671459750/SleepDisturbances?:embed=y&:display_count=yes&:origin=viz_share_link",
         options = {
             hideTabs: true,
             onFirstInteractive: function () {
