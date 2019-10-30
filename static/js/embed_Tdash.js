4219a54daf1b787538c04889fb84b73284333618
ELLOWE <47371493+ELLowe@users.noreply.github.com>
2019-10-29 20:34:55 -0500

    neural network model

--- static/js/embed_Tdash.js
+++ static/js/embed_Tdash.js
@@ -1,7 +1,7 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
         // url = "https://public.tableau.com/views/FinalProjectVisualizations_15719671459750/SleepDisturbances?:embed=y&:display_count=yes&:origin=viz_share_link",
-        url = "https://public.tableau.com/views/AnxietyinPreschoolStudents/Story1?:embed=y&:display_count=yes&:origin=viz_share_link"
+        url = "https://public.tableau.com/shared/DNS48FDHC?:display_count=y&:origin=viz_share_link"
         options = {
             hideTabs: true,
             onFirstInteractive: function () {
